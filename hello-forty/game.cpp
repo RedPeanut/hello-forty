26b711f5f99414e55a2efd7eb8c386b644174168
김진규 <zamong99@gmail.com>
2023-04-23 10:33:01 +0900

    승리후 재시작시 팩 카드 열린채로 보이는 오류 정정

--- hello-forty/game.cpp
+++ hello-forty/game.cpp
@@ -957,6 +957,11 @@ Pack::Pack(int x, int y) : Pile(x, y, 0, 0) {
 
 void Pack::Shuffle() {
 
+    int i;
+    for(i = 0; i <= m_topCard; i++) {
+        m_cards[i]->TurnCard(facedown);
+    }
+
     return;
 
     Card* temp[NumCards];
